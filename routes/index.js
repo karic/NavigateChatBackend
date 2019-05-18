9324f9741e239967ab6eb1a14ccf6fc0f8c1e479
karic <karicvim@gmail.com>
2019-05-18 20:20:48 +0200

    Move location routes from root to api

--- routes/index.js
+++ routes/index.js
@@ -6,46 +6,5 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/locations', function(req,res,next){
-res.send([
-  {
-    "coords": {
-      "accuracy": 65,
-      "altitude": 554.8209838867188,
-      "altitudeAccuracy": 10,
-      "heading": -1,
-      "latitude": 43.84702629718166,
-      "longitude": 18.374295023093808,
-      "speed": -1,
-    },
-    "timestamp": 1558180986026.164,
-  },
-  {
-    "coords": {
-      "accuracy": 65,
-      "altitude": 554.8209838867188,
-      "altitudeAccuracy": 10,
-      "heading": -1,
-      "latitude": 43.94702629718166,
-      "longitude": 18.374295023093808,
-      "speed": -1,
-    },
-    "timestamp": 1558180986026.164,
-  },
-  {
-    "coords": {
-      "accuracy": 65,
-      "altitude": 554.8209838867188,
-      "altitudeAccuracy": 10,
-      "heading": -1,
-      "latitude": 43.84702629718166,
-      "longitude": 18.375295023093808,
-      "speed": -1,
-    },
-    "timestamp": 1558180986026.164,
-  }
 
-  
-]);
-});
 module.exports = router;
